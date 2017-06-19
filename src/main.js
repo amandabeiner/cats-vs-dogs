a5a4512228b3e4b2447d42c35e962480912fa20a
Amanda Beiner <aebeiner@gmail.com>
2017-06-18 21:03:00 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './stylesheets/main.scss';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hi</h1>, document.getElementById('app')
+);
