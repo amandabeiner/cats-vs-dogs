def34f3efdc65f11c8b14f46477baeebdd46777c
Amanda Beiner <aebeiner@gmail.com>
2017-06-19 11:05:53 -0400

    create App component, change data, add foundation

--- src/constants/data.js
+++ src/constants/data.js
@@ -31,21 +31,33 @@ const animals = [
     cats: [
       {
         id: 5,
-        breed: "idk, fluffy?",
+        personality: "equal parts fluff and purr",
         name: "Lina",
         image: "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/12508825_10205738397599624_9202071200945666623_n.jpg?oh=4392c44485ec47178d34830da6b00892&oe=59DC81F5"
       },
       {
         id: 6,
-        breed: "Russian Blue",
+        personality: "wants attention. all of it. always.",
         name: "Rickon",
         image: "http://animalia-life.club/data_images/russian-blue/russian-blue9.jpg"
       },
       {
         id: 6,
-        breed: "the fat black kind",
+        personality: "extra chill",
         name: "Roxy",
         image: "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/17332841_1757115771270001_6200793196046319616_n.jpg"
+      },
+      {
+        id: 7,
+        personality: "very friendly, dumb as a box of hammers",
+        name: "Bushi-cat",
+        image: "https://slack-imgs.com/?c=1&url=https%3A%2F%2Fscontent.cdninstagram.com%2Ft51.2885-15%2Fe35%2F13248922_1765872560308892_2012104964_n.jpg"
+      },
+      {
+        id: 8,
+        personality: "skittish, diabetic, perpetually put-upon",
+        name: "Geisha",
+        image: "https://files.slack.com/files-tmb/T02JFJTNQ-F4S830Q03-333fdd6a66/kitteh_720.png"
       }
     ]
   }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './stylesheets/main.scss';
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data from './constants/data';
+import App from './components/app'
 
 ReactDOM.render(
-  <h1>Hi</h1>, document.getElementById('app')
+  <App data={data}/>, document.getElementById('app')
 );
