f9e239126e6eb2c968db6fc7e78d9fc0f53c6e61
Eben <eben.lowe@gmail.com>
2017-06-20 13:16:59 -0400

    Add event handler functions.

--- src/components/CatSection.js
+++ src/components/CatSection.js
@@ -2,10 +2,16 @@ import React from 'react';
 import CatTile from './CatTile';
 
 const CatSection = props => {
+
+  let catHandler = (catName) => {
+    alert(catName + ' says Meow!')
+  }
+
   let cats = props.cats.map(cat => {
     return(
       <CatTile
         cat={cat}
+        catHandler={catHandler}
         key={cat.id}
       />
     )
--- src/components/CatTile.js
+++ src/components/CatTile.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const CatTile = props => {
+
+  let catClick = event => {
+    props.catHandler(props.cat.name)
+  }
+
   return(
-    <div className="columns small-4">
+    <div className="columns small-4" onClick={catClick}>
       <h4>{props.cat.name}</h4>
       <p>personality: {props.cat.personality}</p>
       <p>human: {props.cat.human}</p>
